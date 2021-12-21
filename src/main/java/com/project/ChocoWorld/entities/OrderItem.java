fb8e1941b50de277b11be904d6bd3e5243a1bea6
AlCad95 <96131356+AlCad95@users.noreply.github.com>
2021-12-21 16:51:12 +0000

    Update OrderItem.java

--- src/main/java/com/project/ChocoWorld/entities/OrderItem.java
+++ src/main/java/com/project/ChocoWorld/entities/OrderItem.java
@@ -1,4 +1,33 @@
 package com.project.ChocoWorld.entities;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "order_item")
 public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_details_id")
+    private OrderDetail orderDetails;
+
+    @OneToOne
+    private Product product;
+
+    private int quantity;
+
+    public OrderItem(OrderDetail orderDetails, Product product, int quantity) {
+        this.orderDetails = orderDetails;
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    // для Thymeleaf
+    public OrderItem() {
+    }
 }
