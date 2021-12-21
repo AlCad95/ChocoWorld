b877fbfe5e36443d6dea7cb7685c56697beb174a
AlCad95 <96131356+AlCad95@users.noreply.github.com>
2021-12-21 16:51:39 +0000

    Update Product.java

--- src/main/java/com/project/ChocoWorld/entities/Product.java
+++ src/main/java/com/project/ChocoWorld/entities/Product.java
@@ -1,4 +1,29 @@
 package com.project.ChocoWorld.entities;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "products")
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String name;
+    private double price;
+    private int inStock;
+
+    public Product() {
+    }
+
+    public Product(String name, double price, int inStock) {
+        this.name = name;
+        this.price = price;
+        this.inStock = inStock;
+    }
+
+
 }
+
